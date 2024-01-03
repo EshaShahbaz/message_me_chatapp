98a9a1c44f2c040330a0bc68ce61de9389054619
EshaShahbaz <144918021+EshaShahbaz@users.noreply.github.com>
2024-01-03 22:27:23 +0500

    Add favicon

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
+
+
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+//= require rails-ujs
+//= require activestorage
+
+
+
 import "@hotwired/turbo-rails"
 import "controllers"
+
