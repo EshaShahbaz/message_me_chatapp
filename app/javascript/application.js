97509384ee5f42c5c179d152309953667e095721
EshaShahbaz <144918021+EshaShahbaz@users.noreply.github.com>
2024-01-13 12:22:26 +0500

    Generate a chatroom channel

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,3 +11,4 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+import "channels"
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get 'logout',to: 'sessions#destroy'
 
   post 'message', to: 'messages#create'
+
+
+  mount ActionCable.server, at: '/cable'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
